d65921d9b0f5fb88bbd28848b6d99f88d2a0e4b6
Casper Oswald <casper@walrusmail.net>
2021-01-17 13:08:50 -0800

    break the web functionality

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
 from flask import Flask, render_template, request, redirect
 
-
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 def index():
+	if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+    	return print(form_data)
+        form_data = request.form
   return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
-
-#@app.route('/form')
-#def form():
-#    return render_template('form.html')
+@app.route('/form')
+def form():
+    return render_template('form.html',form_data = form_data)
  
-#@app.route('/data/', methods = ['POST', 'GET'])
-#def data():
-#    if request.method == 'GET':
-#        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
-#    if request.method == 'POST':
-#        form_data = request.form
-#        return render_template('data.html',form_data = form_data)
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    if request.method == 'GET':
+        return f"The URL /data is accessed directly. Try going to '/form' to submit form"
+    if request.method == 'POST':
+        form_data = request.form
+        return render_template('form.html',form_data = form_data)
 
 if __name__ == '__main__':
   app.run(port=33507)
